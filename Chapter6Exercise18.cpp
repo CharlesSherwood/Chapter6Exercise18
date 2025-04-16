0ab5c59f1d318e6472518d3ddfc8f287bea8dbef
Charles Sherwood <kulan876321@yahoo.com>
2025-04-16 10:11:13 -0400

    Base layout.

--- Chapter6Exercise18.cpp
+++ Chapter6Exercise18.cpp
@@ -7,42 +7,109 @@ how much paint a person needs for a wall
 
 
 #include<cmath>
+#include<string>
 #include<iomanip>
 #include <iostream>
 using namespace std;
 
 void Welcome();
-int GetHoursPrice();
+int GetRooms();
+double GetPrice();
+double GetWall();
+void Calculate(int Rooms,int Price,int Wall);
 bool repeat();
-
+void Display();
  
 
-int main(int Info[2])
+int main()
 {
-	GetHoursPrice();
-	cout << Info[1] << "\n" << Info[0];
+	Welcome();
+	double Rooms, Price, Wall;
+	do
+	{
+		Rooms=GetRooms();
+		Price=GetPrice();
+		Wall=GetWall();
+		Calculate(Rooms, Price, Wall);
+
+
+
+	} while (repeat());
+
 }
+
+int GetRooms()
+{
+	int Rooms;
+	cout << "How Many Rooms Does This Project Have?:";
+	cin >> Rooms;
+	while (Rooms < 1)
+	{
+		cout << "Please Enter A Valid Number";
+		cin >> Rooms;
+	}
+	return Rooms;
+}
+
+double GetPrice()
+{
+	int Price;
+	cout << "How Much Did The Paint Cost?:";
+	cin >> Price;
+	while (Price < 10)
+	{
+		cout << "PLease Enter A Valid Price";
+		cin >> Price;
+	}
+	return Price;
+}
+double GetWall()
+{
+	int Wall;
+	cout << "What Is The Area Of The Wall?:";
+	cin >> Wall;
+	while (Wall < 0)
+	{
+		cout << "PLease Enter A Valid Amount";
+		cin >> Wall;
+	}
+	return Wall;
+
+}
+
+
+void Calculate(int Rooms,int Price,int Wall)
+{
+	int PHour=25, Sfeet=110, Gpaint=1,Labor=8;
+	cout << Rooms << endl;
+	cout << Price << endl;
+	cout << Wall << endl;
+
+
+}
+
 
 
 void Welcome()
 {
-
+	cout << "--------------------------------------\n";
+	cout << "!!Welcome To The Paint Job Estimater!!\n";
+	cout << "--------------------------------------\n";
 }
 
 
-int GetHoursPrice()
-{
-	int Info[2];
-	cout << "Please Enter the amount of hours:";
-	cin >> Info[0];
-	cout << "PLease Enter The Rate That You Are Paying";
-	cin >> Info[1];
-	return Info[2];
-}
 
 bool repeat()
 {	char repeat;
 	cout << "Would You Like To Repeat The Program Y/N:";
 	cin >> repeat;
-	return repeat;
+	return (repeat == 'Y', repeat == 'y');
+}
+
+void Display()
+{
+
+
+
+
 }
