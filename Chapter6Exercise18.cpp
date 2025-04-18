9f1d97ba084d9ff93d830b560bfcaf9cafb9dc81
Charles Sherwood <kulan876321@yahoo.com>
2025-04-18 08:49:15 -0400

    Finished

--- Chapter6Exercise18.cpp
+++ Chapter6Exercise18.cpp
@@ -2,116 +2,138 @@
 ProgrammerName:Charles Sherwood
 Date:4/2025
 Requirements:This program should estimate
-how much paint a person needs for a wall
+How much a paint job for a user inputed number 
+of rooms is based on the companys old jobs.
 */
 
 
-#include<cmath>
-#include<string>
-#include<iomanip>
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 using namespace std;
 
+//The Constant Numbers for the project.
+const double COVERAGE_PER_GALLON = 110.0;
+const double LABOR_HOURS_PER_GALLON = 8.0;
+const double LABOR_COST_PER_HOUR = 25.0;
+
+//Prototypes
 void Welcome();
-double GetRooms();
+int GetRooms();
 double GetPrice();
-double GetWall();
-void Calculate(double Rooms,double Price,double Wall);
+double GetWall(int rooms);
+void Calculate(int rooms, double price, double totalWall);
 bool repeat();
-void Display();
- 
+
 
 int main()
 {
-	Welcome();
-	double Rooms, Price, Wall;
-	do
-	{
-		Rooms=GetRooms();
-		Price=GetPrice();
-		Wall=GetWall();
-		Calculate(Rooms, Price, Wall);
+    Welcome();
+    int rooms;
+    double price, totalWall;
 
-
-
-	} while (repeat());
-
+    do
+    {
+        rooms = GetRooms();
+        price = GetPrice();
+        totalWall = GetWall(rooms);
+        Calculate(rooms, price, totalWall);
+    } while (repeat());
 }
 
-double GetRooms()
-{
-	double Rooms;
-	cout << "How Many Rooms Does This Project Have?:";
-	cin >> Rooms;
-	while (Rooms < 1)
-	{
-		cout << "Please Enter A Valid Number";
-		cin >> Rooms;
-	}
-	return Rooms;
-}
-
-double GetPrice()
-{
-	double Price;
-	cout << "How Much Did The Paint Cost?:";
-	cin >> Price;
-	while (Price < 10)
-	{
-		cout << "PLease Enter A Valid Price";
-		cin >> Price;
-	}
-	return Price;
-}
-
-double GetWall()
-{
-	double Wall;
-	cout << "What Is The Area Of The Wall?:";
-	cin >> Wall;
-	while (Wall < 0)
-	{
-		cout << "PLease Enter A Valid Amount";
-		cin >> Wall;
-	}
-	return Wall;
-
-}
-
-
-void Calculate(double Rooms,double Price,double Wall)
-{
-	int PHour=25, Sfeet=110, Gpaint=1,Labor=8;
-		
-	cout << Rooms << endl;
-	cout << Price << endl;
-	cout << Wall << endl;
-
-
-}
-
-
-
+//Displays A Welcome Meassage
 void Welcome()
 {
-	cout << "--------------------------------------\n";
-	cout << "!!Welcome To The Paint Job Estimater!!\n";
-	cout << "--------------------------------------\n";
+    cout << "----------------------------------------\n";
+    cout << "!! Welcome To The Paint Job Estimator !!\n";
+    cout << "----------------------------------------\n";
 }
 
 
-
-bool repeat()
-{	char repeat;
-	cout << "Would You Like To Repeat The Program Y/N:";
-	cin >> repeat;
-	return (repeat == 'Y' || repeat == 'y');
-}
-
-void Display()
+//Gets The Number Of Rooms
+int GetRooms()
 {
+    int rooms;
+    cout << "How many rooms does this project have?: ";
+    cin >> rooms;
+
+    while (rooms < 1)
+    {
+        cout << "Please enter a valid number of rooms (1 or more): ";
+        cin >> rooms;
+    }
+
+    return rooms;
+}
 
 
+//Gets The Price Of The Paint 
+double GetPrice()
+{
+    double price;
+    cout << "What is the price of the paint per gallon?: $";
+    cin >> price;
+
+    while (price < 10.0)
+    {
+        cout << "Please enter a valid price (at least $10.00): $";
+        cin >> price;
+    }
+
+    return price;
+}
 
 
+//Gets The Square Footage Of The Walls In Each Room
+double GetWall(int rooms)
+{
+    double squareFeet, total = 0;
+
+    for (int i = 1; i <= rooms; i++)
+    {
+        cout << "Enter square footage for room #" << i << ": ";
+        cin >> squareFeet;
+
+        while (squareFeet < 0)
+        {
+            cout << "Please enter a valid (non-negative) square footage: ";
+            cin >> squareFeet;
+        }
+
+        total += squareFeet;
+    }
+
+    return total;
+}
+
+//Calculates Everything And Displays It Onto The Screen
+void Calculate(int rooms, double price, double totalWall)
+{
+  
+    int gallons = ceil(totalWall / COVERAGE_PER_GALLON);
+    double laborHours = gallons * LABOR_HOURS_PER_GALLON;
+    double paintCost = gallons * price;
+    double laborCost = laborHours * LABOR_COST_PER_HOUR;
+    double totalCost = paintCost + laborCost;
+
+    
+    cout << fixed << setprecision(2);
+    cout << "\n----- Paint Job Estimate -----\n";
+    cout << "Total square feet: " << totalWall << " sq ft\n";
+    cout << "Gallons of paint required: " << gallons << endl;
+    cout << "Hours of labor required: " << laborHours << endl;
+    cout << "Cost of the paint: $" << paintCost << endl;
+    cout << "Labor charges: $" << laborCost << endl;
+    cout << "Total cost of the paint job: $" << totalCost << endl;
+    cout << "-------------------------------\n";
+}
+
+
+//Ask The User If They Want To Repeat
+bool repeat()
+{
+    char again;
+    cout << "Would you like to estimate another job? (Y/N): ";
+    cin >> again;
+    return again == 'Y' || again == 'y';
 }
